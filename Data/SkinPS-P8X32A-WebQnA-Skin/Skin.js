44410e5bf1055d13c89b190eb017895f804c075e
Chris Sprague <spragucm@gmail.com>
2019-01-10 17:28:36 +0100

    Propeller Tool Help folder original commit

---
+++ Data/SkinPS-P8X32A-WebQnA-Skin/Skin.js
@@ -0,0 +1,14 @@
+CMCXmlParser._FilePathToXmlStringMap.Add(
+	'Skin',
+	'<?xml version=\"1.0\" encoding=\"utf-8\"?>' +
+	'<CatapultSkin Version=\"1\" Comment=\"Silver theme skin\" Anchors=\"Width,Height\" Width=\"800px\" Height=\"600px\" Top=\"0px\" Left=\"0px\" Bottom=\"0px\" Right=\"0px\" Tabs=\"TOC,Search,Favorites\" DefaultTab=\"TOC\" UseBrowserDefaultSize=\"true\" UseDefaultBrowserSetup=\"true\" Title=\"Propeller Questions &amp; Answers \" AutoSyncTOC=\"true\" conditions=\"PropellerHelpConditionTags.PropellerQNA\" ToolbarInTopic=\"true\">' +
+	'    <!-- saved from url=(0014)about:internet -->' +
+	'    <Index BinaryStorage=\"True\" />' +
+	'    <HtmlHelpOptions ShowMenuBar=\"false\" TopmostWindowStyle=\"False\" Buttons=\"Hide,Locate,Back,Forward,Stop,Refresh,Home,Font,Print\" EnableButtonCaptions=\"True\" AdvancedSearch=\"false\" />' +
+	'    <Stylesheet Link=\"Stylesheet.xml\">' +
+	'    </Stylesheet>' +
+	'    <WebHelpOptions VisibleAccordionItemCount=\"6\" NavigationPaneWidth=\"0\" HideNavigationOnStartup=\"false\" AboutBox=\"About.png\" AboutBoxWidth=\"450\" AboutBoxHeight=\"450\">' +
+	'    </WebHelpOptions>' +
+	'    <Toolbar ExcludeAccordionTitle=\"false\" EnableCustomLayout=\"true\" Buttons=\"ToggleNavigationPane|Home|Separator|Back|Forward|Separator|Print|AddTopicToFavorites|Separator|ExpandAll|CollapseAll|Separator|RemoveHighlight|QuickSearch\" />' +
+	'</CatapultSkin>'
+);
